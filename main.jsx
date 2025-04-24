cc903e35fdb2af224f82264c341aa9619e5ec495
Ian A <iaalvaradocubero@email.neit.edu>
2025-04-24 16:45:25 -0400

    Me cago en fata

---
+++ main.jsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import App from './App';
+// import Products from './components/Products';
+// import Home from './components/Home';
+import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//       <BrowserRouter>
+//       <Routes>
+//           <Route path="/" element={<App />}>
+//             <Route path="home" element={<Home />} />
+//             <Route path="products" element={<Products />} />
+//             <Route path="products/:productId" element={<Products />} />
+//           </Route>
+  
+//           <Route path="*" element={<div>You have been splatted</div>} />
+//         </Routes>
+//       </BrowserRouter>
+//     </React.StrictMode>
+//   );
+  
