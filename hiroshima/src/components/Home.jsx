fe5b5bbbf347996fe5fd4d474fed2ed49f48888c
Ian A <iaalvaradocubero@email.neit.edu>
2025-04-24 17:10:23 -0400

    Me cago en fata

---
+++ hiroshima/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+const Home = () => {
+
+  return (
+    <>
+      <div className="header">This is home</div>
+        <p>Welcome to home</p>
+    </>
+  );
+};
+
+export default Home;
