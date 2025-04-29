77961fa62e50b74616d055dea8759b62aed140a2
Ian A <iaalvaradocubero@email.neit.edu>
2025-04-28 21:23:55 -0400

    Me cago en fata

--- hiroshima/src/Pages/Players.jsx
+++ hiroshima/src/Pages/Players.jsx
@@ -3,27 +3,37 @@ import { Box } from '../components/Box';
 import personas from './personas.json';
 
 const Players = () => {
-    return (
-      <>
-        <div className="header">Hiroshima E-Sports Players</div>
-        <div className="players-grid">
-          {Object.entries(personas).map(([playerName, roles], index) => (
-            <Box key={index} size="lg" className="player-box">
-              <h2>{playerName}</h2>
-              {roles.map((roleObj, idx) => {
-                const roleType = Object.keys(roleObj)[0];
-                const roleNames = roleObj[roleType];
-  
+  return (
+    <>
+      <div className="header">Hiroshima E-Sports Players</div>
+      <div className="players-grid">
+        {Object.entries(personas).map(([playerName, roles], index) => (
+          <Box key={index} size="lg" >
+            <h2 >{playerName}</h2>
+
+            {roles.map((roleObj, idx) => {
+              const roleType = Object.keys(roleObj)[0];
+              const roleNames = roleObj[roleType];
+
+              if (roleType === "Image") {
                 return (
-                  <div key={idx} className="role-entry">
-                    <strong>{roleType.charAt(0).toUpperCase() + roleType.slice(1)}:</strong> {roleNames}
+                  <div key={idx}  >
+                    <img src={process.env.PUBLIC_URL + '/' + roleNames} className="player-image" />
                   </div>
                 );
-              })}
-            </Box>
-          ))}
-        </div>
-      </>
-    );
-  };
+              }
+
+              return (
+                <div key={idx}>
+                  <strong>{roleType.charAt(0).toUpperCase() + roleType.slice(1)}:</strong> {roleNames}
+                </div>
+              );
+            })}
+          </Box>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Players;
