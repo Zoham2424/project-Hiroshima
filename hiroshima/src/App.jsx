39a7800a87831f567489aac546c72923a9ca6d30
Ian A <iaalvaradocubero@email.neit.edu>
2025-04-29 22:59:06 -0400

    a

--- hiroshima/src/App.jsx
+++ hiroshima/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="center">
+      <div >
         <button onClick={playersLookUp} className="button">
           Players
         </button>
