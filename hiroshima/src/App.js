7bd98710d138bf77dd49181d1c56ab63ca2ed0dd
Ian A <iaalvaradocubero@email.neit.edu>
2025-05-26 18:47:28 -0400

    first commit

--- hiroshima/src/App.js
+++ hiroshima/src/App.js
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 import ToggleSwitch from './components/ToggleSwitch';
 import { ThemeContext } from './context/ThemeContext';
 import {useContext } from 'react';
+import logo from './assets/logo.jpg';
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
   return (
     <div className="App">
       <div className="nav-container">
+        <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
       <button onClick={showHome} className="button">Home</button>
         <button onClick={playersLookUp} className="button">Players</button>
         <button onClick={() => navigate('/aboutUs')} className="button">About Us</button>
